b5dfeed7914670e0a6c04a7693fc6901e456c5bd
colinwu <ruiwu1990324@gmail.com>
2013-11-18 16:30:43 -0600

    commit new class Sort, add method quickSort

--- RuiTool/src/com/rui/tools/Test.java
+++ RuiTool/src/com/rui/tools/Test.java
@@ -1,12 +1,16 @@
 package com.rui.tools;
 
 import com.rui.tools.basic.PatternMatcher;
+import com.rui.tools.basic.Sort;
 
 public class Test {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-		PatternMatcher.main(args);
+		// Pattern Matcher Test
+		//PatternMatcher.main(args);
+		//Sort test
+		Sort.main(args);
 	}
 
 }
---
+++ RuiTool/src/com/rui/tools/basic/Sort.java
@@ -0,0 +1,54 @@
+package com.rui.tools.basic;
+
+import java.util.Random;
+
+/**
+ * sort methods
+ * @author ColinMac
+ *
+ */
+public class Sort {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		//random array generator
+		Random ran = new Random(10);
+		int[] a = new int[100];
+		for(int i=0;i<100;i++){
+			a[i]=ran.nextInt(100);
+		}
+		
+		//test area
+		quicksort(a,0,99);
+		
+		//show result
+		for(int i=0;i<100;i++){
+			System.out.print(a[i]+" ");
+		}
+	}
+	
+	// quick sort
+	public static void quicksort(int[] a, int start, int end) throws Exception{    
+		   if (end <= start) 
+			   throw new Exception("input start must be smaller than end");
+		   int left = start;
+		   int right = end;
+		   int pivot = a[left + (right - left) / 2];
+
+		   // selection
+		   while (left <= right) { 
+		       while (a[left] < pivot) left ++; 
+		       while (a[right] > pivot) right --;
+		       if (left <= right) {
+		         int tmp = a[left];
+		         a[left] = a[right];
+		         a[right] = tmp;
+		         left ++;
+		         right --;
+		       }
+		   }
+		   // divide
+		   if (left < end) quicksort(a, left, end);
+		   if (right > start) quicksort(a, start, right);
+		}
+}
